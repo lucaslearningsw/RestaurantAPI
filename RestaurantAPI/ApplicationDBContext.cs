238fd02376b023a6abb0dffccc4c2a771fed82ee
lucas.alixandre <lucas.alixandre@maisinteligencia.com.br>
2021-08-22 13:34:32 -0300

    added references entity core to project and added class DBContext

---
+++ RestaurantAPI/ApplicationDBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+using RestaurantAPI.Models;
+
+namespace RestaurantAPI
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Plates> Plates { get; set; }
+    }
+}
