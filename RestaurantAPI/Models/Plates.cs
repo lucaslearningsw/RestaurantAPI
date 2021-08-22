a056816702fd5296de1710e99b700aa536655583
lucas.alixandre <lucas.alixandre@maisinteligencia.com.br>
2021-08-22 13:15:02 -0300

    create Plates Model

---
+++ RestaurantAPI/Models/Plates.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Models
+{
+    public class Plates
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required] 
+        public string Name{ get; set; }
+
+        [Required] 
+        public float Price{ get; set; }
+
+        [Required] 
+        public string Details { get; set; }
+
+
+    }
+}
