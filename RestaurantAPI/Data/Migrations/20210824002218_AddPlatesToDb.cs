4d028946ef9a1f8b065a965b7c4fd56a50437388
lucas.alixandre <lucas.alixandre@maisinteligencia.com.br>
2021-08-23 21:35:38 -0300

    added first migration to DataBase

---
+++ RestaurantAPI/Data/Migrations/20210824002218_AddPlatesToDb.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RestaurantAPI.Migrations
+{
+    public partial class AddPlatesToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Plates",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<float>(type: "real", nullable: false),
+                    Details = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Plates", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Plates");
+        }
+    }
+}
