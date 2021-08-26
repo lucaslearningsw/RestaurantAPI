b9f9750fdee16da59fcd55294555c0742ddfe6d1
lucas.alixandre <lucas.alixandre@maisinteligencia.com.br>
2021-08-26 07:58:11 -0300

    added controller Plate

---
+++ RestaurantAPI/Controllers/PlateController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using RestaurantAPI.Repository.IRepository;
+
+namespace RestaurantAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlateController : Controller
+    {
+        private  IPlatesRepository _plateRepo;
+        private readonly IMapper _mapper;
+
+        public PlateController(IPlatesRepository plateRepo, IMapper mapper)
+        {
+            _plateRepo = plateRepo;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetPlates()
+        {
+            var objList = _plateRepo.GetPlates();
+            return Ok(objList);
+        }
+    }
+}
